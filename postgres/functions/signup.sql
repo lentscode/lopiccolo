47e721aaa0d69f7918cd787cf5222ae07a11afb4
Lentscode <antonio.lentini2004@gmail.com>
2024-12-02 17:40:56 +0100

    🗃️ Update `signup` function

--- postgres/functions/signup.sql
+++ postgres/functions/signup.sql
@@ -2,11 +2,19 @@ CREATE OR REPLACE FUNCTION signup (
   _email VARCHAR(255),
   _hash_password VARCHAR(255)
 )
-RETURNS BIGINT
+RETURNS TABLE (
+  email VARCHAR(255),
+  id BIGINT
+)
 AS $$
   BEGIN
-    SELECT id FROM users
-    WHERE email = _email AND
-    hash_password = _hash_password;
+    RETURN QUERY
+    INSERT INTO users AS u
+    (email, hash_password)
+    VALUES (_email, _hash_password)
+    RETURNING u.email, u.id;
+
+    -- HANDLE EXCEPTIONS 
   END
+$$
 LANGUAGE plpgsql;
